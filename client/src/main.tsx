e2154087f24d3790016bed4a9350784a996de4a8
TayyibahHq <66921146+TayyibahHq@users.noreply.github.com>
2026-07-23 13:18:35 -0500

    fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Restore path after 404.html redirect (GitHub Pages SPA routing)
+if (sessionStorage.redirect) {
+  const path = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, "", path);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
